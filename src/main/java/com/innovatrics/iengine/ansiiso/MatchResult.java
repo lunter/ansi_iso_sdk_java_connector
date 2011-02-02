421e4188a4ea1e4b78f402cf153778a3bb3366dc
Martin Vysny <martin.vysny@innovatrics.com>
2011-02-02 14:32:33 +0100

    Fixed javadoc - match result quality

--- src/main/java/com/innovatrics/iengine/ansiiso/MatchResult.java
+++ src/main/java/com/innovatrics/iengine/ansiiso/MatchResult.java
@@ -1,6 +1,5 @@
 package com.innovatrics.iengine.ansiiso;
 
-import java.util.Collections;
 import java.util.List;
 
 /**
@@ -9,7 +8,7 @@ import java.util.List;
  */
 public final class MatchResult {
     /**
-     * Quality score, 0..100.
+     * Quality score, 0..31.
      */
     public final int score;
     public final int dx;
